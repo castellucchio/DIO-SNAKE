17be723b25ae355afd44efad8e6858e83d7bbadc
Diego Amaro Castellucchio <castellucchiosoftware@gmail.com>
2021-06-16 13:43:58 -0300

    criado funcionalidade de geração de comida aleatória no mapa

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ snake[0] ={
     x: 8 * box,
     y: 8 * box
 }
+
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 let direction = "right";
 
 function criarBG()
@@ -21,6 +26,11 @@ function criarSnake()
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function criarComida(){
+    context.fillStyle = "red";
+    context.fillRect(food.x,food.y,box,box);
 }
 
 document.addEventListener('keydown',update);
@@ -48,7 +58,7 @@ function iniciarJogo()
         snake[0].y = 16 * box;
     criarBG();
     criarSnake();
-
+    criarComida();
     let snakeX =snake[0].x;
     let snakeY =snake[0].y;
 
