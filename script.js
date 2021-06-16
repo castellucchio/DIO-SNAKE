f8a35d1730812e9177227cff73fa3932a63a1201
Diego Amaro Castellucchio <castellucchiosoftware@gmail.com>
2021-06-16 13:00:46 -0300

    criado a snake

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 
 let box = 32;
+let snake = [];
+snake[0] ={
+    x: 8 * box,
+    y: 8 * box
+}
 
 function criarBG()
 {
@@ -9,4 +14,14 @@ function criarBG()
     context.fillRect(0,0,16 * box, 16 * box);
 }
 
+function criarSnake()
+{
+    for(i=0; i <= snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y,
+            box,box);
+    }
+}
+
 criarBG();
+criarSnake();
